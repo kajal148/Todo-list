654d92da33c492ed4e164aa41dd29afabdc6b2f3
soham pingat <soham.pingat@gmail.com>
2020-10-19 21:57:42 +0530

    Added date

--- To-do list/assets/js/script.js
+++ To-do list/assets/js/script.js
@@ -1,3 +1,11 @@
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+today = mm + '/' + dd + '/' + yyyy;
+
+document.getElementById('date').textContent = today;
+
 $("ul").on('click', 'li', function(){
 	$(this).toggleClass("completed")
 })
